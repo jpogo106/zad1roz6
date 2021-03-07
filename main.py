5760e05d4774d28d456de09b35f3ae6a01668db8
123 <puste54@wp.pl>
2021-03-07 15:30:18 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+#Zad1
+#lista = ['Holiday','Friends','Teoria wielkiego podrywu']
+#lista.reverse()
+#lista.append('Weselny tydzien')
+#lista.append('Zabojczy rejs')
+#lista.append('Garfield')
+#lista.append('Obecnosc)
+
+#Zad2
+#kluczem jest rezyser
+#slownik = {Nancy Meyers:'Holiday',Robert Smigel:'Weselny tydzien',Kyle Newacheck:'zabojczy rejs',Peter Hewitt, Tim Hill:'Garfield',James Wan:'Obecnosc'}
+#slownik[Nancy Meyers] = 'Holiday'
+#slownik[Robert Smigel] = 'Weselny tydzien'
+#slownik[Kyle Newacheck] = 'zabojczy rejs'
+#slownik[Peter Hewitt, Tim Hill] = 'Garfield'
+#slownik[James Wan] = 'Obecnosc'
+#print(slownik[Nancy Mayers])
+
+
+#Zad3
+#przedmioty = {'CAD':'CAD Komputerowe Wspomaganie Projektowania','ps':'Programowanie Strukturalne','emd':'elementy matematyki dyskretnej','wd':'wizualizacja danych'}
+#x = len(przedmioty)
+#print(x)
+
+
+#Zad4
+#liczba = input("podaj liczbę: ")
+#liczba = int(liczba)
+#liczba **= liczba
+#print(liczba)
+
+#Zad5
+#import sys as system
+#system.stdout.write("podaj ciag znakow: ")
+#napis = system.stdin.readline()
+#ile = napis.count('a')
+#print('jest ',ile,' znaków "a" w napisie')
+
+#Zad8
+#zadanie = []
+#pom = 0
+#while pom != 10:
+#    wartosc = input("podaj liczbę: ")
+#    wartosc = float(wartosc)
+#    pom += 1
+#    if (wartosc%1 == 0):
+#        wartosc = int(wartosc)
+#        zadanie.append(wartosc)
+#
+#print(zadanie)
